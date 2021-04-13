894a3fd8d7f955215678ca7e77e96187a5396622
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-13 19:50:03 +0530

    test

--- python_data_science/matplotlib_package.py
+++ python_data_science/matplotlib_package.py
@@ -29,19 +29,29 @@ plt.savefig('cases_per_month.png')
 df.groupby('month')['deaths'].sum().plot(kind='bar')
 plt.savefig('deaths_per_month.png')
 
-df = df.groupby('month')[['cases', 'deaths']].sum()
-df.plot(kind='bar', stacked=True)
+df.groupby('month')[['cases', 'deaths']].sum().plot(kind='bar', stacked=True)
 plt.savefig('cases_deaths_per_month.png')
 
-df = df.groupby('month')[['cases', 'deaths']].sum()
-df.plot(kind='barh', stacked=True)
+df.groupby('month')[['cases', 'deaths']].sum().plot(kind='barh', stacked=True)
 plt.savefig('cases_deaths_per_month_horizontal.png')
 
-# df[df['month'] == 6]['cases'].plot(kind='box')
-# plt.savefig('boxplot.png')
-#
-# df[df['month'] == 6]['cases'].plot(kind='hist')
-# plt.savefig('histogram_1.png')
-#
-# df[df['month'] == 6]['cases'].plot(kind='hist', bins=10)
-# plt.savefig('histogram_2.png')
+df[df['month'] == 6]['cases'].plot(kind='box')
+plt.savefig('boxplot.png')
+
+df[df['month'] == 6]['cases'].plot(kind='hist')
+plt.savefig('histogram_1.png')
+
+df[df['month'] == 6]['cases'].plot(kind='hist', bins=10)
+plt.savefig('histogram_2.png')
+
+df[df['month'] == 6][['cases', 'deaths']].plot(kind='area', stacked=False)
+plt.savefig('area_not_stacked.png')
+
+df[df['month'] == 6][['cases', 'deaths']].plot(kind='area')
+plt.savefig('area_stacked.png')
+
+df[df['month'] == 6][['cases', 'deaths']].plot(kind='scatter', x='cases', y='deaths')
+plt.savefig('scatter.png')
+
+df.groupby('month')['cases'].sum().plot(kind='pie')
+plt.savefig('pie.png')
