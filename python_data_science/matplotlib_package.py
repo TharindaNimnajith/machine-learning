0cb85620f72869c54baf56bc740bb87beea45300
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-13 18:16:53 +0530

    test

--- python_data_science/matplotlib_package.py
+++ python_data_science/matplotlib_package.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Matplotlib is a library used to create graphs, charts, and figures. It also provides functions to customize your
-# figures by changing the colors and labels.
+# Matplotlib is a library used to create graphs, charts, and figures.
+# It also provides functions to customize your figures by changing the colors and labels.
 help(plt)
 print(dir(plt))
 print(plt)
@@ -10,3 +10,13 @@ print(plt)
 s = pd.Series([18, 42, 9, 32, 81, 64, 3])
 s.plot(kind='bar')
 plt.savefig('plot.png')
+
+df = pd.read_csv('./ca-covid.csv')
+df.drop('state', axis=1, inplace=True)
+df['date'] = pd.to_datetime(df['date'], format='%d.%m.%y')
+df['month'] = df['date'].dt.month
+df.set_index('date', inplace=True)
+df[df['month'] == 12]['cases'].plot()
+plt.savefig('cases.png')
+df[df['month'] == 12]['cases', 'deaths'].plot()
+plt.savefig('cases_deaths.png')
