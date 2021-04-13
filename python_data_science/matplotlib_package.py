72dceb492bb960b8fe13679e1afe60dddd0eaabe
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-13 19:57:16 +0530

    test

--- python_data_science/matplotlib_package.py
+++ python_data_science/matplotlib_package.py
@@ -9,7 +9,7 @@ print(plt)
 
 s = pd.Series([18, 42, 9, 32, 81, 64, 3])
 s.plot(kind='bar')
-plt.savefig('plot.png')
+plt.savefig('./plots/plot.png')
 
 df = pd.read_csv('./ca-covid.csv')
 df.drop('state', axis=1, inplace=True)
@@ -18,40 +18,40 @@ df['month'] = df['date'].dt.month
 df.set_index('date', inplace=True)
 
 df[df['month'] == 12]['cases'].plot()
-plt.savefig('cases.png')
+plt.savefig('./plots/cases.png')
 
 df[df['month'] == 12][['cases', 'deaths']].plot()
-plt.savefig('cases_deaths.png')
+plt.savefig('./plots/cases_deaths.png')
 
 df.groupby('month')['cases'].sum().plot(kind='bar')
-plt.savefig('cases_per_month.png')
+plt.savefig('./plots/cases_per_month.png')
 
 df.groupby('month')['deaths'].sum().plot(kind='bar')
-plt.savefig('deaths_per_month.png')
+plt.savefig('./plots/deaths_per_month.png')
 
 df.groupby('month')[['cases', 'deaths']].sum().plot(kind='bar', stacked=True)
-plt.savefig('cases_deaths_per_month.png')
+plt.savefig('./plots/cases_deaths_per_month.png')
 
 df.groupby('month')[['cases', 'deaths']].sum().plot(kind='barh', stacked=True)
-plt.savefig('cases_deaths_per_month_horizontal.png')
+plt.savefig('./plots/cases_deaths_per_month_horizontal.png')
 
 df[df['month'] == 6]['cases'].plot(kind='box')
-plt.savefig('boxplot.png')
+plt.savefig('./plots/boxplot.png')
 
 df[df['month'] == 6]['cases'].plot(kind='hist')
-plt.savefig('histogram_1.png')
+plt.savefig('./plots/histogram_1.png')
 
 df[df['month'] == 6]['cases'].plot(kind='hist', bins=10)
-plt.savefig('histogram_2.png')
+plt.savefig('./plots/histogram_2.png')
 
 df[df['month'] == 6][['cases', 'deaths']].plot(kind='area', stacked=False)
-plt.savefig('area_not_stacked.png')
+plt.savefig('./plots/area_not_stacked.png')
 
 df[df['month'] == 6][['cases', 'deaths']].plot(kind='area')
-plt.savefig('area_stacked.png')
+plt.savefig('./plots/area_stacked.png')
 
 df[df['month'] == 6][['cases', 'deaths']].plot(kind='scatter', x='cases', y='deaths')
-plt.savefig('scatter.png')
+plt.savefig('./plots/scatter.png')
 
 df.groupby('month')['cases'].sum().plot(kind='pie')
-plt.savefig('pie.png')
+plt.savefig('./plots/pie.png')
