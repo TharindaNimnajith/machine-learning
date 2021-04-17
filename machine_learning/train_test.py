882a0eb3ce805ad64ecb299b94f68030335185e5
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-18 02:37:43 +0530

    test

--- machine_learning/train_test.py
+++ machine_learning/train_test.py
@@ -12,19 +12,14 @@ y = df['Survived'].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-print('Whole dataset:', X.shape, y.shape)
-print('Training set:', X_train.shape, y_train.shape)
-print('Test set:', X_test.shape, y_test.shape)
-
 # building the model
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
 # evaluating the model
-# print('Accuracy:', model.score(X_test, y_test))
+print('Accuracy:', model.score(X_test, y_test))
 y_pred = model.predict(X_test)
-print('Accuracy:', accuracy_score(y, y_pred))
-print('Precision:', precision_score(y, y_pred))
-print('Recall:', recall_score(y, y_pred))
-print('F1 Score:', f1_score(y, y_pred))
-print(confusion_matrix(y, y_pred))
+print('Accuracy:', accuracy_score(y_test, y_pred))
+print('Precision:', precision_score(y_test, y_pred))
+print('Recall:', recall_score(y_test, y_pred))
+print('F1 Score:', f1_score(y_test, y_pred))
