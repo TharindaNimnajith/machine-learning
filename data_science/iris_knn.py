2e07536ababde59be03f4e59d541a686a0d5bb1d
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-18 19:57:51 +0530

    test

--- data_science/iris_knn.py
+++ data_science/iris_knn.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_matrix
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score, GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
 
 iris = pd.read_csv('csv_files/iris.csv')
@@ -41,3 +42,20 @@ print(confusion_matrix(y_test, pred, labels=['iris-setosa', 'iris-versicolor', '
 print(plot_confusion_matrix(knn, X_test, y_test, cmap=plt.cm.Blues))
 plt.savefig('plots/confusion_matrix.png')
 plt.show()
+
+cross_validation_scores = cross_val_score(knn, X, y, cv=5)
+print(cross_validation_scores)
+print(cross_validation_scores.mean())
+
+knn2 = KNeighborsClassifier()
+param_grid = {
+    'n_neighbors': np.arange(2, 10)
+}
+knn_gscv = GridSearchCV(knn2, param_grid, cv=5)
+knn_gscv.fit(X, y)
+print(knn_gscv.best_params_)
+print(knn_gscv.best_score_)
+knn_final = KNeighborsClassifier(n_neighbors=knn_gscv.best_params_['n_neighbors'])
+knn_final.fit(X, y)
+y_pred = knn_final.predict(X)
+print(knn_final.score(X, y))
