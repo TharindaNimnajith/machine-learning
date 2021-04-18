b249068149db3492e8fb4f3f49e0bb99787d5bd4
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-18 21:09:04 +0530

    test

--- data_science/k_means_clustering.py
+++ data_science/k_means_clustering.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.datasets import load_wine
@@ -17,3 +18,14 @@ kmeans.fit(X_scaled)
 
 y_pred = kmeans.predict(X_scaled)
 print(y_pred)
+
+print(kmeans.cluster_centers_)
+
+plt.scatter(X_scaled[:, 0], X_scaled[:, 1], c=y_pred)
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], marker='*', s=250, c=[0, 1, 2],
+            edgecolors='k')
+plt.xlabel('alcohol')
+plt.ylabel('total phenols')
+plt.title('k-means (k=3)')
+plt.savefig('plots/k_means.png')
+plt.show()
