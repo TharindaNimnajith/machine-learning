324346d134c64e4137992307151777e4c7957ce5
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-18 20:44:28 +0530

    test

---
+++ data_science/clustering_wines.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from pandas.plotting import scatter_matrix
+from sklearn.datasets import load_wine
+
+x1 = np.array([0, 1])
+x2 = np.array([2, 0])
+
+print(np.sqrt(((x1 - x2) ** 2).sum()))
+print(np.sqrt(5))
+
+data = load_wine()
+wine = pd.DataFrame(data.data, columns=data.feature_names)
+
+print(wine.shape)
+print(wine.columns)
+print(wine.info())
+print(wine.describe())
+print(wine.iloc[:, :5].describe())
+print(wine.head())
+print(wine.tail())
+
+scatter_matrix(wine.iloc[:, [0, 5]])
+plt.savefig('plots/wine_scatter.png')
+plt.show()
