84bd92224e39b4f5cec5b9117adbdb57d64557ef
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-18 05:38:49 +0530

    test

--- data_science/data_visualization.py
+++ data_science/data_visualization.py
@@ -1,6 +1,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 help(mpl)
 print(dir(mpl))
@@ -68,6 +69,25 @@ plt.plot(x, np.sin(x), 'k:', label='sin(x)')
 plt.plot(x, np.cos(x), 'r--', label='cos(x)')
 plt.legend()
 plt.savefig('plots/sin_cos.png')
+# plt.draw()
+# plt.pause(1)
+# plt.show()
+# plt.clf()
+
+# presidents_df = pd.read_csv('csv_files/president_heights_party.csv', index_col='name')
+# plt.scatter(presidents_df['height'], presidents_df['age'])
+# plt.savefig('plots/presidents_scatter.png')
+# plt.draw()
+# plt.pause(1)
+# plt.show()
+# plt.clf()
+
+presidents_df = pd.read_csv('csv_files/president_heights_party.csv', index_col='name')
+plt.scatter(presidents_df['height'], presidents_df['age'], marker='^')
+plt.xlabel('age')
+plt.ylabel('height')
+plt.title('US Presidents')
+plt.savefig('plots/presidents_scatter_v2.png')
 # plt.draw()
 # plt.pause(1)
 # plt.show()
