7bc525fcbd707ab46a2ba26c0f669a7b51349843
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-18 18:41:07 +0530

    test

--- data_science/classification.py
+++ data_science/classification.py
@@ -7,8 +7,8 @@
 # print(type(iris))
 # print(iris.shape)
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 # iris = pd.read_csv('https://sololearn.com/uploads/files/iris.csv')
 iris = pd.read_csv('csv_files/iris.csv')
@@ -18,11 +18,33 @@ print(iris.head())
 iris.drop('id', axis=1, inplace=True)
 print(iris.head())
 print(iris.describe())
-print(iris[['petal_len','petal_wd']].describe())
+print(iris[['petal_len', 'petal_wd']].describe())
 print(iris.groupby('species').size())
 print(iris['species'].value_counts())
 
 iris.hist()
 print(iris.hist())
 plt.savefig('plots/iris_hist.png')
+plt.show()
+
+inv_name_dict = {
+    'iris-setosa': 0,
+    'iris-versicolor': 1,
+    'iris-virginica': 2
+}
+
+colors = [inv_name_dict[item] for item in iris['species']]
+
+scatter = plt.scatter(iris['sepal_len'], iris['sepal_wd'], c=colors)
+plt.xlabel('sepal length (cm)')
+plt.ylabel('sepal width (cm)')
+plt.legend(handles=scatter.legend_elements()[0], labels=inv_name_dict.keys())
+plt.savefig('plots/iris_scatter_sepal.png')
+plt.show()
+
+scatter = plt.scatter(iris['petal_len'], iris['petal_wd'], c=colors)
+plt.xlabel('petal length (cm)')
+plt.ylabel('petal width (cm)')
+plt.legend(handles=scatter.legend_elements()[0], labels=inv_name_dict.keys())
+plt.savefig('plots/iris_scatter_petal.png')
 plt.show()
