f5d57963c2802993c159f59611b331328c80e588
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-22 05:56:24 +0530

    test

---
+++ machine_learning/k_fold_cross_validation.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import KFold
+
+df = pd.read_csv('csv_files/titanic.csv')
+
+X = df[['Age', 'Fare']].values[:6]
+y = df['Survived'].values[:6]
+
+kf = KFold(n_splits=3, shuffle=True)
+
+splits = list(kf.split(X))
+print(splits)
+
+first_split = splits[0]
+train_indices, test_indices = first_split
+print('Training set indices:', train_indices)
+print('Test set indices:', test_indices)
+
+X_train = X[train_indices]
+X_test = X[test_indices]
+y_train = y[train_indices]
+y_test = y[test_indices]
+print(X_train)
+print(y_train)
+print(X_test)
+print(y_test)
--- machine_learning/roc_curve.py
+++ machine_learning/roc_curve.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import precision_score, recall_score, precision_recall_fscore_support, roc_curve
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import precision_recall_fscore_support, roc_curve, roc_auc_score
 from sklearn.model_selection import train_test_split
 
 sensitivity_score = recall_score
@@ -12,7 +13,6 @@ def specificity_score(y_true, y_pred):
     return r[0]
 
 
-# df = pd.read_csv('https://sololearn.com/uploads/files/titanic.csv')
 df = pd.read_csv('csv_files/titanic.csv')
 
 df['male'] = df['Sex'] == 'male'
@@ -26,6 +26,11 @@ model = LogisticRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
+
+print('Accuracy: {0:.5f}'.format(accuracy_score(y_test, y_pred)))
+print('Precision: {0:.5f}'.format(precision_score(y_test, y_pred)))
+print('Recall: {0:.5f}'.format(recall_score(y_test, y_pred)))
+print('F1 Score: {0:.5f}'.format(f1_score(y_test, y_pred)))
 
 print('Sensitivity:', sensitivity_score(y_test, y_pred))
 print('Specificity:', specificity_score(y_test, y_pred))
@@ -48,3 +53,13 @@ plt.xlabel('1 - specificity')
 plt.ylabel('sensitivity')
 plt.savefig('plots/roc_curve.png')
 plt.show()
+
+model1 = LogisticRegression()
+model1.fit(X_train, y_train)
+y_pred_proba1 = model1.predict_proba(X_test)
+print('Model 1 AUC Score:', roc_auc_score(y_test, y_pred_proba1[:, 1]))
+
+model2 = LogisticRegression()
+model2.fit(X_train[:, 0:2], y_train)
+y_pred_proba2 = model2.predict_proba(X_test[:, 0:2])
+print('Model 2 AUC Score:', roc_auc_score(y_test, y_pred_proba2[:, 1]))
