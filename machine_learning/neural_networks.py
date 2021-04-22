a6de2383325eeaaf11a940f04d916b2edb31d209
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-04-22 20:53:51 +0530

    test

--- machine_learning/neural_networks.py
+++ machine_learning/neural_networks.py
@@ -1,1 +1,11 @@
-#
+from sklearn.datasets import make_classification
+from matplotlib import pyplot as plt
+
+X, y = make_classification(n_features=2, n_redundant=0, n_informative=2, random_state=3)
+print(X)
+print(y)
+
+plt.scatter(X[y == 0][:, 0], X[y == 0][:, 1], s=100, edgecolors='k')
+plt.scatter(X[y == 1][:, 0], X[y == 1][:, 1], s=100, edgecolors='k', marker='^')
+plt.savefig('plots/make_classification.png')
+plt.show()
